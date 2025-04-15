96527319bd1462dd7e93fe808499d387b3e86b1c
drsmych <staydeepbro@gmail.com>
2025-04-14 23:09:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from "react";
+import Homepage from "./components/homepage";
 
 function App() {
-  const playSound = () => {
-      const audio = new Audio("/media/button-click.mp3");
-      audio.play();
-  };
-
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>🔊 Audio Test</h1>
-      <button onClick={playSound}>Click me to play sound</button>
-    </div>
-  );
+  return <Homepage />;
 }
 
 export default App;
-
