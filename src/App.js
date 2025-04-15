abd75dde99bb1ba3d538a778d506e4e917f43998
Derrell Smith <himself@drudysmith.com>
2025-04-14 20:58:51 -0700

    Use Plasmic Homepage as root component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PlasmicHomepage from "./components/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Look here and save to regenerate.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Boom!
-        </a>
-      </header>
-    </div>
+      <Homepage />
   );
 }
 
 export default App;
+
+
