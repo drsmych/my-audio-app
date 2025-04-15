ac9d1e1f3bf9cbb8029efbfae3e2817deb54b0e2
Derrell Smith <himself@drudysmith.com>
2025-04-14 23:01:22 -0700

    Hook up Plasmic component ID's for logic and audio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Homepage from "./components/Homepage";
 
 function App() {
+  const playSound = () => {
+      const audio = new Audio("/media/button-click.mp3");
+      audio.play();
+  };
+
   return (
-    <Homepage />
+    <div style={{ padding: "2rem" }}>
+      <h1>🔊 Audio Test</h1>
+      <button onClick={playSound}>Click me to play sound</button>
+    </div>
   );
 }
 
 export default App;
-
 
